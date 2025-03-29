f99e87dc883e60d8981b5c5b9b4e76f8466cf119
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-03-29 15:11:26 +0600

    updaet

--- src/Component/home/HRated.jsx
+++ src/Component/home/HRated.jsx
@@ -41,21 +41,26 @@ const HRated = () => {
                 {
                     hRate.map(HRate => (
                         <div key={HRate._id}>
-                            <div className="card bg-base-100 my-4 w-[300px] md:w-96 xl:w-80 shadow-xl">
-                                <figure>
-                                    <img
-                                        className='h-[260px]'
-                                        src={HRate.Image}
-                                        alt="Shoes" />
-                                </figure>
-                                <div className="card-body">
-                                    <h2 className="card-title">{HRate.name}</h2>
-                                    <p>{HRate.Hading}</p>
-                                    <p>Rating {HRate.Rating}⭐</p>
-                                    <div className="card-actions justify-end">
-                                        <button className="btn btn-primary"
-                                        onClick={() => handleExploreDetails(HRate)}>Explore Details</button>
+                            {/* navigat ui */}
+                            <div className="w-full max-w-[340px] space-y-3 rounded-xl bg-white p-4 shadow-lg dark:bg-[#18181B]">
+                                <div className="relative flex h-48 w-full justify-center lg:h-[260px]">
+                                    <div className="absolute left-4 right-4 top-4 flex items-center justify-between">
+                                        {/* love  */}
+                                        <div className="flex items-center">
+                                            <svg width={30} className="cursor-pointer fill-transparent stroke-white stroke-2 hover:fill-red-500 hover:stroke-red-500" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M2 9.1371C2 14 6.01943 16.5914 8.96173 18.9109C10 19.7294 11 20.5 12 20.5C13 20.5 14 19.7294 15.0383 18.9109C17.9806 16.5914 22 14 22 9.1371C22 4.27416 16.4998 0.825464 12 5.50063C7.50016 0.825464 2 4.27416 2 9.1371Z"></path></svg>
+                                        </div>
                                     </div>
+                                    <img width={400} height={400} className="rounded-lg bg-black/40 object-cover" src={HRate.Image} alt="card navigate ui" />
+                                </div>
+                                <div className="space-y-2 font-semibold">
+                                    <h6 className="text-sm md:text-base lg:text-lg">{HRate.name}</h6>
+                                    <p className="text-xs font-semibold text-gray-400 md:text-sm">{HRate.Hading}</p>
+                                    <p>Rating {HRate.Rating} ⭐</p>
+                                </div>
+                                <div className="flex flex-wrap items-center justify-between gap-6 text-sm md:text-base">
+                                    <button 
+                                    onClick={() => handleExploreDetails(HRate)}
+                                    className="rounded-lg bg-[#49B2FF] px-4 py-2 font-semibold text-white duration-300 hover:scale-105 hover:bg-sky-600">View Details</button>
                                 </div>
                             </div>
                         </div>
