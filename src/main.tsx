816b3686e969f2b57cbf0135588d1b7124ceb5da
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-08-13 16:20:25 +0600

    update to blue up my foot

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+// import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
 import './index.css'
 import App from './App.tsx'
+import { Toaster } from "react-hot-toast";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = document.getElementById("root") as HTMLElement;
+
+ReactDOM.createRoot(root).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+    </Routes>
+    <Toaster/>
+  </BrowserRouter>,
+);
+
